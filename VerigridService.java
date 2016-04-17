3013f7d610e4ced750ccdb9295d02a3a96cc4300
hhwhite1019 <hhwhite1019@hotmail.com>
2016-04-17 09:45:41 -0400

    Add IsAlive method
    This method checks the vital signs of the service

--- VerigridService.java
+++ VerigridService.java
@@ -3,7 +3,14 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
+public interface IsAlive {
+
+	@WebMethod public String sayHelloWorld(String name);
+}
+
+@WebService
 public interface HelloWorld {
 
 	@WebMethod public String sayHelloWorld(String name);
 }
+
